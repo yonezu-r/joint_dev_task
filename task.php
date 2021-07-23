6a75c7924e0eece1e2fe9fae221701edd119e2c5
yonezu-r <alfoot39@icloud.com>
2021-07-23 15:10:49 +0900

    Q8まで回答

--- task.php
+++ task.php
@@ -6,6 +6,9 @@ $names = ["田中", "佐藤", "佐々木", "高橋"];
 
   # 以下に回答を記載
 
+  array_push($names,"斎藤");
+  print_r($names);
+
 echo PHP_EOL;
 
 print("#####q2#####".PHP_EOL);
@@ -14,6 +17,10 @@ $array2 = ["bird", "bat", "tiger"];
 
   # 以下に回答を記載
 
+  $array = array_merge(array1,array2);
+
+print_r($array);
+
 echo PHP_EOL;
 
 print("#####q3#####".PHP_EOL);
@@ -21,12 +28,24 @@ $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 1, 4, 5, 9];
 
   # 以下に回答を記載
 
+$count = 0;
+foreach($numbers as $number){
+if($number === 3){
+$count++;
+}
+}
+print_r($count . "回".PHP_EOL);
+
 echo PHP_EOL;
 
 print("#####q4#####".PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
   # 以下に回答を記載
+
+$sports = array_diff($sports,[null]);
+
+print_r($sports);
 
 echo PHP_EOL;
 
@@ -36,6 +55,9 @@ $array2 = [1, 5, 8, 10];
 
   # 以下に回答を記載
 
+var_export(empty($array1));
+echo PHP_EOL;
+var_export(empty($array2));
 echo PHP_EOL;
 
 print("#####q6#####".PHP_EOL);
@@ -43,14 +65,24 @@ $numbers1 = [1, 2, 3, 4, 5];
 
   # 以下に回答を記載
 
+$numbers2 = [];
+//$numbers2を定義
+//foreach作成 $numbers1の中に$numberを定義 $numbers1×10を$numberを代入していく。array_pushで$numberを$numbers2に代入していく。$numbers2をプリント。ßß
+foreach($numbers1 as $number){
+$number *= 10;
+array_push($numbers2,$number);
+}
+print_r($numbers2);
+
 echo PHP_EOL;
 
 print("#####q7#####".PHP_EOL);
 $array = ["1", "2", "3", "4", "5"];
 
-  # 以下に回答を記載
-
-  # 以下は変更しないで下さい
+# 以下に回答を記載
+//??
+$array = array_map('intval',$array);
+# 以下は変更しないで下さい
 var_dump($array);
 
 echo PHP_EOL;
@@ -59,6 +91,8 @@ print("#####q8#####".PHP_EOL);
 $programming_languages = ["php", "ruby", "python", "javascript"];
 
   # 以下に回答を記載
+$programming_languages = array_map('ucfirst',$programming_languages );
+$upper_case_programming_languages= array_map('strtoupper',$programming_languages );
 
   # 以下は変更しないで下さい
 print_r($programming_languages);
